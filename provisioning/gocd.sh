a890d3c6049b65723cc3b29eb8b2d6540fb2985f
cortiz <cortiz@thoughtworks.com>
2017-06-06 12:11:52 -0500

    [#7] with @scrodrig, @xnaunay - changed vault configuration files

---
+++ provisioning/gocd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#SETUP GOCD
+# run the following command in infraestructura folder
+docker-compose up -d
--- provisioning/vault.sh
+++ provisioning/vault.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SETUP VAULT
-docker-compose create
+cd ../vault-server
 docker-compose up -d
 sudo apt-get install unzip
 wget https://releases.hashicorp.com/vault/0.7.2/vault_0.7.2_linux_amd64.zip
@@ -11,10 +11,10 @@ vault init > keys.txt
 for i in $(awk  '/Unseal/ {print $4}' keys.txt); do vault unseal "$i"; done
 awk  '/Initial Root/ {print $4}' keys.txt | xargs vault auth
 vault auth-enable cert
-vault policy-write leave-team ./vault-server/policies/default-team-policy.hcl
-for i in `ls ./vault-server/users/certs/*.crt`; do
-	filewithextension=$(basename "$i")
-	filename="${filewithextension%.*}"
+vault policy-write leave-team ./policies/default-team-policy.hcl
+for i in `ls ./users/certs/*.crt`; do
+        filewithextension=$(basename "$i")
+        filename="${filewithextension%.*}"
   vault write auth/cert/certs/$filewithextension display_name="$filename" policies=leave-team certificate=@$i ttl=3600;
 done
 rm vault_0.7.2_linux_amd64.zip*
